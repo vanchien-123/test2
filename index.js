6c4c5aa41b9dfe28d7af3b528418efc665b3c6d4
Admin <admin@Admins-MacBook-Pro.local>
2023-03-12 20:39:27 +0700

    add more files

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
+app.use(express.static("public"));
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    // res.send('Hello World')
+    res.set({
+	"Allow-access-Allow-Origin": "*",
+    });
+    return res.redirect("index.html");
 })
 
 app.listen(3000)
